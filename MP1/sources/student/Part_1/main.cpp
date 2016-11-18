12b2b8ea6006592097460d5170e311b5354de4ea
andrew.kirfman <andrew.kirfman@tamu.edu>
2016-11-17 21:39:09 -0600

    Stuff and things.  Still  working on MP1

--- MP1/sources/student/Part_1/main.cpp
+++ MP1/sources/student/Part_1/main.cpp
@@ -13,37 +13,46 @@ int main(int argc, char ** argv)
 	
 	char * msg = "a sample message";
 	
-	Init (M,b); // initialize
-	// test operations
+    linked_list* test_list = new linked_list();
+
+	test_list->Init(M,b); // initialize
+	
+    // test operations
 	int testnums [] = {100, 5, 200, 7, 39, 25, 400, 50, 200, 300};
 	int i = 0;
-	// some sample insertions
+	
+    // some sample insertions
 	for (i=0; i< 10; i ++)
 	{
-		Insert (testnums [i], buf, 50);   // insert 50 bytes from the buffer as value for each of the insertions
+		test_list->Insert(testnums [i], buf, 50);   // insert 50 bytes from the buffer as value for each of the insertions
 	}
-	Insert (150, buf, 200); // this Insert should fail
-	PrintList ();
-	Delete (7);
-	Insert (13, msg, strlen(msg)+1);		// insertion of strings, copies the null byte at the end
-	Delete (55);
-	Insert (15, "test msg", 8);
-	Delete (3);
-	PrintList ();
+	
+    // This Insert should fail
+    test_list->Insert (150, buf, 200);
+
+	test_list->PrintList ();
+	test_list->Delete (7);
+	test_list->Insert (13, msg, strlen(msg)+1);		// insertion of strings, copies the null byte at the end
+	test_list->Delete (55);
+	test_list->Insert (15, "test msg", 8);
+	test_list->Delete (3);
+	test_list->PrintList ();
 	
 	// a sample lookup operations that should return null, because it is looking up a non-existent number
-	char* kv = Lookup (3);
+	char* kv = test_list->Lookup (3);
 	if (kv)
+    {
 		printf ("Key = %d, Value Len = %d\n", *(int *) kv, *(int *) (kv+4));
-	
-	
+    }
+
 	// this look up  should succeed and print the string "a sample message"
 	kv = Lookup (13);
 	if (kv)
+    {
 		printf ("Key = %d, Value Len = %d, Value = %s\n", *(int *) kv, *(int *) (kv+4), kv + 8);
-	
+    }
 	
 	// end test operations	
-	Destroy ();
+	test_list->Destroy ();
 	
 }

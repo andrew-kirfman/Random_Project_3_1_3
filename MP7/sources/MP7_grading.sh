6f2e6effff31ba9a00ac3da06a7d5677afe80c53
jhigginbotham64 <jhigginbotham64@gmail.com>
2017-01-02 11:58:46 -0600

    Learning python, gdb, gdb-python, Eclipse, and Vocareum all at once is a little intimidating, but progress is being made! Changes are related to the building of MP7_grading.sh and MP7_grading_python_source.py.

--- MP7/sources/MP7_grading.sh
+++ MP7/sources/MP7_grading.sh
@@ -1,2 +1,2 @@
-make clean all
-gdb -q client_solution -x MP7_grading_python_source.py > grading_output.txt 2>&1
+#make clean all
+gdb -q client_solution -x MP7_grading_python_source.py 2>&1 | tee grading_output.txt 
--- MP7/sources/MP7_grading_python_source.py
+++ MP7/sources/MP7_grading_python_source.py
@@ -2,4 +2,14 @@ import sys
 import gdb
 from subprocess import call
 
-gdb.execute('quit')
+def stop_handler (event):
+    if event.__class__.__name__ == "BreakpointEvent":
+        for bp in event.breakpoints:
+            print("Hit breakpoint at " + bp.location)
+    gdb.execute("cont")
+
+rtf_break = gdb.Breakpoint("request_thread_function", gdb.BP_BREAKPOINT, 0, False, False)
+wtf_break = gdb.Breakpoint("worker_thread_function", gdb.BP_BREAKPOINT, 0, False, False)
+gdb.events.stop.connect (stop_handler)
+gdb.execute("run -v 3 -w 40 -n 100")
+gdb.execute("quit")
